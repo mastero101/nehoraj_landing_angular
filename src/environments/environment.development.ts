6ec590198ef585338d9e89dcd9573b3b6ab0de64
mastero101 <mastershift_101@hotmail.com>
2025-06-17 17:48:37 -0600

    feat(nehoraj): mejorar diseño y funcionalidad del componente
    Se actualizaron estilos en el componente Nehoraj, incluyendo mejoras en la navegación y en las tarjetas de soluciones. Se añadieron animaciones y se ajustaron los colores para una mejor experiencia visual. Además, se configuró el entorno de desarrollo con claves de API y credenciales para producción.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    openaiApiKey: 'apikeyOpenAI',
+    meta: {
+      phoneNumberId: 'tu_phone_number_id_produccion',
+      accessToken: 'tu_access_token_produccion'
+    },
+    twilio: {
+      accountSid: 'tu_account_sid_produccion',
+      authToken: 'tu_auth_token_produccion',
+      whatsappNumber: 'tu_whatsapp_number_produccion'
+    }
+};
